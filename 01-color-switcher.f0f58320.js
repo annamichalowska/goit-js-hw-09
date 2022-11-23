bf5f89ba2af814cd42ff7db320c1312883740ca7
annamichalowska <107001736+annamichalowska@users.noreply.github.com>
2022-11-23 20:31:03 +0000

    Deploying to gh-pages from @ annamichalowska/goit-js-hw-09@5797aeedc7d655d8febb4ab08e6fe90e7c1d934e 🚀

--- 01-color-switcher.f0f58320.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",(()=>{o=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(o),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.f0f58320.js.map
