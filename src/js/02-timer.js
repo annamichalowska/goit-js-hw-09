4d6a096c2112b5b1f9f03b0470a76712bb6532c5
Anna <michalowska.aneczka@gmail.com>
2022-11-24 22:52:46 +0100

    counter_half_task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,31 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const buttonStart = document.querySelector('button[data-start]');
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+//console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+//console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+//console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 const options = {
   enableTime: true,
@@ -9,9 +33,8 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
     if (selectedDates[0] < new Date()) {
-      alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
       buttonStart.disabled = true;
     } else {
       buttonStart.disabled = false;
@@ -21,10 +44,10 @@ const options = {
 
 flatpickr('#datetime-picker', { ...options });
 
-const newData = options.defaultDate;
-console.log(newData);
-console.log(newData.getTime());
+// const newData = options.defaultDate;
+// console.log(newData);
+// console.log(newData.getTime());
 
-const actualData = new Date();
-console.log(actualData);
-console.log(actualData.getTime());
+// const actualData = new Date();
+// console.log(actualData);
+// console.log(actualData.getTime());
