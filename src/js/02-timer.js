eb3b2aa85af33862e225f47a88b1e1fb822ea1fe
Anna <michalowska.aneczka@gmail.com>
2022-11-24 22:15:41 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,8 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const buttonStart = document.querySelector('button[data-start]');
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -12,3 +14,20 @@ const options = {
 };
 
 flatpickr('#datetime-picker', { ...options });
+const choiceData = () => {
+  const newData = options.defaultDate;
+  const actualData = new Date();
+  console.log(newData);
+  console.log(actualData);
+  console.log(newData.getTime());
+  console.log(actualData.getTime());
+  if (newData.getTime() < actualData.getTime()) {
+    console.log('ZŁA DATA!!!!!');
+    buttonStart.disabled = false;
+  } else {
+    console.log('DOBRA DATA!!!!');
+    buttonStart.disabled = true;
+  }
+};
+
+//console.clear();
