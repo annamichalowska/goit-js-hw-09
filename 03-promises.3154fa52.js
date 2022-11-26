38b18db6a3c1e21828e45c9fb849b0a073adb36c
annamichalowska <107001736+annamichalowska@users.noreply.github.com>
2022-11-26 18:59:37 +0000

    Deploying to gh-pages from @ annamichalowska/goit-js-hw-09@1f4fd4f41c36e895acb5066b7743a145f7f30986 🚀

---
+++ 03-promises.3154fa52.js
@@ -0,0 +1,2 @@
+(void Math.random()).then((({position:i,delay:e})=>{Notify.failure(`✅ Fulfilled promise ${i} in ${e}ms`)})).catch((({position:i,delay:e})=>{Notify.failure(`❌ Rejected promise ${i} in ${e}ms`)}));
+//# sourceMappingURL=03-promises.3154fa52.js.map
---
+++ 03-promises.6ea2c8c0.js
@@ -0,0 +1,2 @@
+(void Math.random()).then((function(i){var o=i.position,n=i.delay;Notify.failure("✅ Fulfilled promise ".concat(o," in ").concat(n,"ms"))})).catch((function(i){var o=i.position,n=i.delay;Notify.failure("❌ Rejected promise ".concat(o," in ").concat(n,"ms"))}));
+//# sourceMappingURL=03-promises.6ea2c8c0.js.map
