aea3c35daa9a27f41bf0321c386a3012328b36eb
annamichalowska <107001736+annamichalowska@users.noreply.github.com>
2022-11-28 16:56:13 +0000

    Deploying to gh-pages from @ annamichalowska/goit-js-hw-09@bd4058aa94673ecfedca9e40b48c07fdc92d9357 🚀

--- 01-color-switcher.38a62a65.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;e.disabled=!0,t.addEventListener("click",(function(){n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),e.disabled=!0,t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.38a62a65.js.map
