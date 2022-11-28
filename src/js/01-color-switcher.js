5f8b059f2bf261a35554072a06984f183cd1603b
Anna <michalowska.aneczka@gmail.com>
2022-11-28 17:11:45 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,8 @@ const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 let timerId = null;
 
+buttonStop.disabled = true;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -11,12 +13,14 @@ buttonStart.addEventListener('click', () => {
     let colorValue = getRandomHexColor();
     document.body.style.backgroundColor = colorValue;
   }, 1000);
+
   buttonStart.disabled = true;
   buttonStop.disabled = false;
 });
 
 buttonStop.addEventListener('click', () => {
   clearInterval(timerId);
+
   buttonStop.disabled = true;
   buttonStart.disabled = false;
 });
